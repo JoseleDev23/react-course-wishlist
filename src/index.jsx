8d10e4b4febe62ceb64bbb7790eef70f3c4e1ba3
Yodel23 <dawjosele@gmail.com>
2024-10-28 15:47:51 -0400

    CursoReact - Intalacion entorno y  aplicacion creada

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Versiones antiguas a React 18
+// ReactDOM.render(
+//     <div>
+//         <h1>Wishlist</h1>
+//     </div>,
+//     document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div>
+        <h1>Wishlist</h1>
+    </div>
+);
